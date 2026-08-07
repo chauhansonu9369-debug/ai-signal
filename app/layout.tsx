8bb8a0ba1abb92eabfbd5274b85e4521fa94e1ec
chauhansonu9369-debug <chauhansonu9369@gmail.com>
2026-08-07 16:53:38 +0530

    Add Cloudflare Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +30,12 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <Analytics />
+      
+        <Script
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          strategy="afterInteractive"
+          data-cf-beacon='{"token":"1cacaea177604286a82128d0449cea0a"}'
+        />
       </body>
     </html>
   );
